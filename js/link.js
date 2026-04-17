366b4f8ea2295397d066873380ad7f7593b1a1ae
adavice <97439259+adavice@users.noreply.github.com>
2026-04-17 16:00:44 +0200

    Update link.js

--- js/link.js
+++ js/link.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const trigger2 = document.querySelector(".trigger2");
     if (
         document
-            .querySelectorAll('a[href="/?page=login"]')
+            .querySelector('a[href="/?page=login"]')
             .hasAttribute("style", "display:none")
     ) {
         console.log("Login link is hidden");
