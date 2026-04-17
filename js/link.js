c9937b650aa22876aaf15bcb47a4de39b7803576
adavice <97439259+adavice@users.noreply.github.com>
2026-04-17 15:57:30 +0200

    fixed logout menu

---
+++ js/link.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const trigger1 = document.querySelector(".trigger1");
+    const trigger2 = document.querySelector(".trigger2");
+    if (
+        document
+            .querySelectorAll('a[href="/?page=login"]')
+            .hasAttribute("style", "display:none")
+    ) {
+        console.log("Login link is hidden");
+        trigger1.parentElement.setAttribute(
+            "style",
+            "display:block; position:relative;"
+        );
+        trigger2.parentElement.setAttribute(
+            "style",
+            "display:block; position:relative;"
+        );
+    }
+});
